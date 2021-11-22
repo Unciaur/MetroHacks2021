df848278f8deda1269ab1a7f97a2179ddc971d2d
Powerpower8888 <52894100+Powerpower8888@users.noreply.github.com>
2021-11-22 08:56:26 -0600

    Update FifteenPuzzle.java

--- FifteenPuzzle.java
+++ FifteenPuzzle.java
@@ -12,12 +12,12 @@ import javax.swing.JButton;
  *
  * @author jennifer
  */
-public class MetroHacks2021 extends javax.swing.JFrame {
+public class FifteenPuzzle extends javax.swing.JFrame {
 
     /**
-     * Creates new form MetroHacks2021
+     * Creates new form FifteenPuzzle
      */
-    public MetroHacks2021() {
+    public FifteenPuzzle() {
         initComponents();
     }
 
@@ -702,7 +702,7 @@ public class MetroHacks2021 extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -716,13 +716,13 @@ public class MetroHacks2021 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MetroHacks2021.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FifteenPuzzle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MetroHacks2021.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FifteenPuzzle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MetroHacks2021.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FifteenPuzzle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MetroHacks2021.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FifteenPuzzle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
